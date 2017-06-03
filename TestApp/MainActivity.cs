bd49d20eb0e14aa8d5a940f3d723854585c0ba9d
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2017-06-03 01:48:18 -0400

    initial version
    built against https://github.com/TapTrack/TappyBLE/commit/24ed778c4c451201f8b95858b201e72b901c00a9

---
+++ TestApp/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Taptrack.Tcmptappy.Commlink.Ble;
+using Android.Runtime;
+using Com.Taptrack.Tcmptappy.Tappy.Ble.Scanner;
+using Com.Taptrack.Tcmptappy.Tappy.Ble;
+
+namespace TestApp
+{
+    [Activity(Label = "TestApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        private ITappyBleDeviceDefinition deviceDefinition;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += Button_Click;
+        }
+
+        void Button_Click(object sender, System.EventArgs e)
+        {
+            TappyBleCommunicator communicator =
+                new TappyBleCommunicator(this, deviceDefinition);
+            //prepare to connect
+            communicator.Initialize();
+            //connect to the tappy
+            communicator.Connect();
+            // work with tappy
+            // disconnect from the tappy
+            communicator.Dispose();
+            // close the connection
+            // this cancels the connection and removes all listeners
+            communicator.Close();
+            var scanner = TappyBleScanner.Get();
+        }
+    }
+}
+
